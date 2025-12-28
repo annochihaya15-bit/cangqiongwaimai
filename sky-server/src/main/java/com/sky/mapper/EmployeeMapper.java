5fed35299ae85b0f7c24b7b2059f69bcc9255a15
annochihaya15-bit <3087359568@qq.com>
2025-12-28 16:04:48 +0800

    苍穹外卖项目

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -5,14 +5,15 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
-public interface EmployeeMapper {
+public interface EmployeeMapper extends com.baomidou.mybatisplus.core.mapper.BaseMapper<Employee>{
 
     /**
      * 根据用户名查询员工
+     *
      * @param username
      * @return
      */
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
-
 }
+
